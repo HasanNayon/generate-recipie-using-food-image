2afc8429eb5c7579200f30020f01e3d71ecaef34
Mohammad Hamim <hamimmd555@gmail.com>
2024-05-12 11:36:58 +0800

    local work from nayon

---
+++ backend/main.py
@@ -0,0 +1,50 @@
+import streamlit as st
+import tensorflow as tf
+import numpy as np
+
+
+
+#Tensorflow Model Prediction
+def model_prediction(test_image):
+    model = tf.keras.models.load_model("image_to_food_model1.h5")
+    image = tf.keras.preprocessing.image.load_img(test_image,target_size=(256,256))
+    input_arr = tf.keras.preprocessing.image.img_to_array(image)
+    input_arr = np.array([input_arr]) #convert single image to batch
+    predictions = model.predict(input_arr)
+    return np.argmax(predictions) #return index of max element
+
+
+#Sidebar
+st.sidebar.title("Dashboard")
+app_mode = st.sidebar.selectbox("Select Page",["Home","Prediction"])
+
+#Main Page
+if(app_mode=="Home"):
+    st.header("Food Recipe")
+    image_path = "home_page.jpg"
+    st.image(image_path)
+
+
+#Prediction Page
+elif(app_mode=="Prediction"):
+    st.header("Model Prediction")
+    test_image = st.file_uploader("Choose an Image:")
+    if(st.button("Show Image")):
+        st.image(test_image,width=4,use_column_width=True)
+    
+    #Predict button
+    if(st.button("Predict")):
+        st.snow()
+        st.write("Our Prediction")
+        result_index = model_prediction(test_image)
+        #Reading Labels
+        with open("labels.txt") as f:
+            content = f.readlines()
+        label = []
+        
+        for i in content:
+            label.append(i[:-1])
+        st.success("Model is Predicting it's a {}".format(label[result_index]))
+
+    
+
