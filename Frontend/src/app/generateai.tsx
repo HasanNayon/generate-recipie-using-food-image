5aba737ff0032441c756143d6f99bad7f59e55d4
Mohammad Hamim <hamimmd555@gmail.com>
2024-05-12 14:00:56 +0800

    linked generateai page

--- Frontend/src/app/generateai.tsx
+++ Frontend/src/app/generateai.tsx
@@ -1,10 +1,53 @@
-const generateAi = () => {
-    return (
-      <div>
-      <p> asdfads </p>
-      </div>
-    );
+"use client"
+
+import React, { useState } from 'react';
+
+function GenerateAi() {
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files;
+    if (files && files.length > 0) {
+      setSelectedFile(files[0]);
+    } else {
+      setSelectedFile(null);
+    }
   };
-  
-  export default generateAi;
-  
+
+  const handlePredict = async () => {
+    if (!selectedFile) {
+      alert('Please select an image first');
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('file', selectedFile);
+
+    try {
+      const response = await fetch('http://localhost:8501', {
+        method: 'POST',
+        body: formData
+      });
+      
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      const data = await response.json();
+      console.log("Here is maal: ", data.prediction);
+      // setPrediction(data.prediction);
+    } catch (error) {
+      console.error('There was an error!', error);
+    }
+  };
+
+  return (
+    <div>
+      <input type="file" onChange={handleFileChange} />
+      <button onClick={handlePredict}>Predict</button>
+      {/* Render prediction here */}
+    </div>
+  );
+}
+
+export default GenerateAi;
